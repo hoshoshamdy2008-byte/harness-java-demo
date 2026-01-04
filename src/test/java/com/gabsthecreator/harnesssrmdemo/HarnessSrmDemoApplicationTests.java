2c3e77e5b95bf27785f5cef2f98b51edb27425b8
hoshoshamdy2008-byte <hoshoshamdy2008@gmail.com>
2026-01-04 22:39:12 +0400

    Update HarnessSrmDemoApplicationTests.java

--- src/test/java/com/gabsthecreator/harnesssrmdemo/HarnessSrmDemoApplicationTests.java
+++ src/test/java/com/gabsthecreator/harnesssrmdemo/HarnessSrmDemoApplicationTests.java
@@ -1,20 +1,30 @@
 package com.gabsthecreator.harnesssrmdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+// THESE ARE THE MISSING IMPORTS CAUSING THE ERROR:
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HarnessSrmDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc; // This solves the "cannot find symbol: variable mockMvc"
 
-	@Test
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
     void shouldReturnDefaultMessage() throws Exception {
-        // This tests if the root URL "/" returns a 200 OK status
+        // The imports above allow you to use get() and status()
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk());
     }
-
 }
