0dc18d9044ba4ab0383ea1cb071cf7c7b84d80cc
hoshoshamdy2008-byte <hoshoshamdy2008@gmail.com>
2026-01-04 22:29:56 +0400

    Update HarnessSrmDemoApplicationTests.java

--- src/test/java/com/gabsthecreator/harnesssrmdemo/HarnessSrmDemoApplicationTests.java
+++ src/test/java/com/gabsthecreator/harnesssrmdemo/HarnessSrmDemoApplicationTests.java
@@ -10,4 +10,11 @@ class HarnessSrmDemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+    void shouldReturnDefaultMessage() throws Exception {
+        // This tests if the root URL "/" returns a 200 OK status
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
 }
